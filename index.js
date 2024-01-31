760ef15a86ae20dabcd517f4f13feb96f02d0680
Allan Daniel Saenz <allan.d.saenz@gmail.com>
2024-01-31 15:51:44 -0500

    create dog cards that will be placed on DOM

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => fetchData);
+//ON DOM LOAD...
+document.addEventListener('DOMContentLoaded',function(){
+//Fetch dog images
+
+
+
+//create dog cards...==>
+function createDogCards(dog){
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+    <img src="${dog.dogUrl}">
+    <div class="content">
+        <h4>${dog.breed}</h4>
+        <p>${dog.description}</p>
+    </div>
+    `
+//==>...and render dog to DOM
+document.querySelector('#dogs').appendChild(card)
+}
+console.log(createDogCards)
+
+
+});
+
 
 
 
